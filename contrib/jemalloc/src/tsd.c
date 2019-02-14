ded1542c0deaa436128b890ef1fffbf7cf63ce00
Bill Sorenson <instructionset@gmail.com>
2019-02-13 21:19:37 -0600

    Revert "Update jemalloc to version 4.0.4."
    This reverts commit edc319ed79837a53b421d478b95557670e810469.

--- contrib/jemalloc/src/tsd.c
+++ contrib/jemalloc/src/tsd.c
@@ -73,9 +73,6 @@ tsd_cleanup(void *arg)
 	tsd_t *tsd = (tsd_t *)arg;
 
 	switch (tsd->state) {
-	case tsd_state_uninitialized:
-		/* Do nothing. */
-		break;
 	case tsd_state_nominal:
 #define O(n, t)								\
 		n##_cleanup(tsd);
