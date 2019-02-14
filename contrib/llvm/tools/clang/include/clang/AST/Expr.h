8d961c2c5dc68a939d4c72ddb66e8207b3025ff1
Bill Sorenson <instructionset@gmail.com>
2019-02-14 14:55:40 -0600

    Revert "﻿MFC r327930:"
    This reverts commit af874530b0f44fb960658f50bf0b3f9b8cf3dd33.

--- contrib/llvm/tools/clang/include/clang/AST/Expr.h
+++ contrib/llvm/tools/clang/include/clang/AST/Expr.h
@@ -3986,10 +3986,6 @@ public:
   /// initializer)?
   bool isTransparent() const;
 
-  /// Is this the zero initializer {0} in a language which considers it
-  /// idiomatic?
-  bool isIdiomaticZeroInitializer(const LangOptions &LangOpts) const;
-
   SourceLocation getLBraceLoc() const { return LBraceLoc; }
   void setLBraceLoc(SourceLocation Loc) { LBraceLoc = Loc; }
   SourceLocation getRBraceLoc() const { return RBraceLoc; }
@@ -3998,9 +3994,6 @@ public:
   bool isSemanticForm() const { return AltForm.getInt(); }
   InitListExpr *getSemanticForm() const {
     return isSemanticForm() ? nullptr : AltForm.getPointer();
-  }
-  bool isSyntacticForm() const {
-    return !AltForm.getInt() || !AltForm.getPointer();
   }
   InitListExpr *getSyntacticForm() const {
     return isSemanticForm() ? AltForm.getPointer() : nullptr;
