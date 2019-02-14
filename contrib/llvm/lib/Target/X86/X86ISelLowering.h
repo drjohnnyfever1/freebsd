cdffec5ebf5172547f8de61d9c99820012840699
Bill Sorenson <instructionset@gmail.com>
2019-02-14 15:57:14 -0600

    Revert "MFC r332833:"
    This reverts commit 98598cf3f04eb78a3c59c5fa6f6a12c1d67be752.

--- contrib/llvm/lib/Target/X86/X86ISelLowering.h
+++ contrib/llvm/lib/Target/X86/X86ISelLowering.h
@@ -1099,6 +1099,9 @@ namespace llvm {
     bool lowerInterleavedStore(StoreInst *SI, ShuffleVectorInst *SVI,
                                unsigned Factor) const override;
 
+
+    void finalizeLowering(MachineFunction &MF) const override;
+
   protected:
     std::pair<const TargetRegisterClass *, uint8_t>
     findRepresentativeClass(const TargetRegisterInfo *TRI,
