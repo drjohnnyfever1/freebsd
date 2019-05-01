a07f022048fcfae7360a0a9732472523992387d3
Bill Sorenson <instructionset@gmail.com>
2019-04-30 20:52:47 -0500

    Revert "MFC r344444:"
    This reverts commit 471beb5bb603641aeddffcd4b804fa62e0d0892c.

--- contrib/llvm/tools/lld/ELF/SyntheticSections.cpp
+++ contrib/llvm/tools/lld/ELF/SyntheticSections.cpp
@@ -1282,8 +1282,6 @@ template <class ELFT> void DynamicSection<ELFT>::finalizeContents() {
   }
   if (!Config->ZText)
     DtFlags |= DF_TEXTREL;
-  if (Config->HasStaticTlsModel)
-    DtFlags |= DF_STATIC_TLS;
 
   if (DtFlags)
     addInt(DT_FLAGS, DtFlags);
